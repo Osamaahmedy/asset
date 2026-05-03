6c0f1a2285b749b40834b6062a88778936b9145b
osamaahmedy <asaam4292@gmail.com>
2026-05-04 00:10:58 +0300

    data4

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable, HasRoles;
 public function departments()
@@ -45,7 +46,10 @@ public function departments()
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
+public function canAccessPanel(Panel $panel): bool
+{
+    return true;
+}
     /**
      * العلاقة مع الأدوار (هذه العلاقة مضافة تلقائياً بفضل HasRoles trait,
      * لكن لو حبيت تضيفها بشكل يدوي، كالتالي:)
